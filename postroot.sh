0b7679b83bc76a94957824c0b4aa581208355eca
Wörsty <git@loxberry.woerstenfeld.de>
2019-05-15 00:14:50 +0200

    Update postroot.sh

--- postroot.sh
+++ postroot.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 echo `date +"%b  %e %H:%M:%S "`"Check, if APC UPS Daemon is running"                                                                                          
 CS_PID=`ps -ef|grep "/sbin/apcupsd"|grep -v grep |awk -F" " '{ print $2 }'`                                                                           
@@ -9,6 +10,12 @@ else
 	kill -9 $CS_PID;
 fi
 	
+ARGV0=$0 # Zero argument is shell command
+ARGV1=$1 # First argument is temp folder during install
+ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
+ARGV3=$3 # Third argument is Plugin installation folder
+ARGV4=$4 # Forth argument is Plugin version
+ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 echo `$ARGV5/system/daemons/plugins/$ARGV2`
 
 exit 0
