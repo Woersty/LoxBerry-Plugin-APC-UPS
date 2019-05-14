27a6f0eea9588d4676b7579c18fdac1707de5c97
Wörsty <git@loxberry.woerstenfeld.de>
2019-05-15 00:15:48 +0200

    Update postroot.sh

--- postroot.sh
+++ postroot.sh
@@ -16,6 +16,6 @@ ARGV2=$2 # Second argument is Plugin-Name for scipts etc.
 ARGV3=$3 # Third argument is Plugin installation folder
 ARGV4=$4 # Forth argument is Plugin version
 ARGV5=$5 # Fifth argument is Base folder of LoxBerry
-echo `$ARGV5/system/daemons/plugins/$ARGV2`
+$ARGV5/system/daemons/plugins/$ARGV2
 
 exit 0
